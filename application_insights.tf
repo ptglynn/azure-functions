0b40396e2431cbb48e9933fa3f86c5f2f86039d3
ptglynn <pglynn@paloaltonetworks.com>
2020-08-19 16:44:45 -0500

    Delete application_insights.tf

--- application_insights.tf
+++
@@ -1,6 +0,0 @@
-resource "azurerm_application_insights" "application_insights" {
-  name                = "service-tags"
-  location            = azurerm_resource_group.resource-group.location
-  resource_group_name = azurerm_resource_group.resource-group.name
-  application_type    = "web"
-}
