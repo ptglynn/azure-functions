cf0e9817afd831efd0ebd35f8a9bb1174a6d2a5a
ptglynn <pglynn@paloaltonetworks.com>
2020-08-19 16:45:34 -0500

    Delete resource_group.tf

--- resource_group.tf
+++
@@ -1,7 +0,0 @@
-# ********** RESOURCE GROUP **********
-
-# Create a resource group
-resource "azurerm_resource_group" "resource-group" {
-  name     = "${var.rg_base_name}-${random_pet.random_pet.id}"
-  location = var.region
-}
